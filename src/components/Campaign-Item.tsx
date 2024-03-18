e157d2362f75b08c25df89e04f9080a663575e9c
ste1v0 <tyotarjous@yandex.ru>
2024-03-18 18:46:56 +0300

    Fix typing

--- src/components/Campaign-Item.tsx
+++ src/components/Campaign-Item.tsx
@@ -26,24 +26,20 @@ function CampaignItem({ id, planetName, initialOwner, liberation, players } : { 
                         : <Skeleton width={'4rem'}/>
                     }
                 </Typography>
-                {players &&
-                    <>
-                            {!isLoading 
-                            ? <>
+                    {!isLoading && liberation
+                        ? <>
                             <Typography color={'#FFF'} fontWeight={'200'} sx={{ fontSize: 14, mb: 0.5}}>
                                 Liberation: {liberation === 100 || liberation === 0 ? `${Math.ceil(liberation)}%` : `${liberation?.toFixed(2)}%`}
                             </Typography>
                             <Box component='div' sx={{backgroundColor: '#FFF', position: 'relative', width: '100%', height: 20, mb: 2}}>
-                                <Box component={'div'} sx={{backgroundColor: '#BB86FC', position: 'absolute', width: `${Math.ceil(liberation)}%`, height: '100%'}}>
-                                </Box>
+                                <Box component={'div'} sx={{backgroundColor: '#BB86FC', position: 'absolute', width: `${Math.ceil(liberation)}%`, height: '100%'}} />
                             </Box>
-                            </>
-                            : <Skeleton width={'4rem'} />}
+                        </>
+                        : <Skeleton width={'4rem'} />
+                    }
                         <Typography color={'#9B9B9B'} fontWeight={'200'} sx={{ fontSize: 14 }} gutterBottom>
-                            {!isLoading ? `Players: ${internationalize(players)}` : <Skeleton width={'4rem'} />}
+                            {!isLoading && players ? `Players: ${internationalize(players)}` : <Skeleton width={'4rem'} />}
                         </Typography>
-                    </>
-                }
             </CardContent>
         </Card>
     )
